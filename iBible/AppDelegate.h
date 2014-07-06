2dc6eefc33a8c520bc4f7094bcae0d8a28e6f850
Zulus <vgzulus@gmail.com>
2014-07-06 08:34:59 +0600

    first commit

---
+++ iBible/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBible
+//
+//  Created by вадим on 7/6/14.
+//  Copyright (c) 2014 вадим. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iBibleTests/iBibleTests.m
@@ -0,0 +1,34 @@
+//
+//  iBibleTests.m
+//  iBibleTests
+//
+//  Created by вадим on 7/6/14.
+//  Copyright (c) 2014 вадим. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iBibleTests : XCTestCase
+
+@end
+
+@implementation iBibleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
